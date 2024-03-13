6d41b2dbe0e17c66b1c2a922ebd8202c96aa6480
Jens Walter <jens@apimeister.com>
2024-03-13 14:40:10 +0100

    dtz base crate

---
+++ dtz/src/lib.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, Clone)]
+pub struct Configuration {
+    pub base_path: String,
+    pub client: reqwest::Client,
+    pub api_key: Option<String>,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            base_path: "https://dtz.rocks".to_string(),
+            client: Default::default(),
+            api_key: Default::default(),
+        }
+    }
+}
