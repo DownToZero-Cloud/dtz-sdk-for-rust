339a05ad3bf2b459f51ce0f5b93ae62e129e3d5b
Jens Walter <jens@apimeister.com>
2024-03-23 11:08:34 +0100

    refine types

--- dtz-core/docs/CreateContext200Response.md
+++ dtz-core/docs/CreateContext200Response.md
@@ -4,8 +4,8 @@
 
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**context_id** | Option<**String**> |  | [optional]
-**created** | Option<**String**> |  | [optional]
+**context_id** | [**uuid::Uuid**](uuid::Uuid.md) |  | 
+**created** | **String** |  | 
 **alias** | Option<**String**> |  | [optional]
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- dtz-core/src/models/create_context_200_response.rs
+++ dtz-core/src/models/create_context_200_response.rs
@@ -14,19 +14,19 @@ use crate::models;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct CreateContext200Response {
-    #[serde(rename = "contextId", skip_serializing_if = "Option::is_none")]
-    pub context_id: Option<String>,
-    #[serde(rename = "created", skip_serializing_if = "Option::is_none")]
-    pub created: Option<String>,
+    #[serde(rename = "contextId")]
+    pub context_id: uuid::Uuid,
+    #[serde(rename = "created")]
+    pub created: String,
     #[serde(rename = "alias", skip_serializing_if = "Option::is_none")]
     pub alias: Option<String>,
 }
 
 impl CreateContext200Response {
-    pub fn new() -> CreateContext200Response {
+    pub fn new(context_id: uuid::Uuid, created: String) -> CreateContext200Response {
         CreateContext200Response {
-            context_id: None,
-            created: None,
+            context_id,
+            created,
             alias: None,
         }
     }
