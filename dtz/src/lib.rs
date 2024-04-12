512ab98ca7f514d71c793ac7848778cee0d0f950
Jens Walter <jens@apimeister.com>
2024-04-12 23:53:22 +0200

    public mods

--- dtz/src/lib.rs
+++ dtz/src/lib.rs
@@ -1,31 +1,31 @@
 pub use dtz_config::*;
 
 #[cfg(feature = "containers")]
-mod containers {
+pub mod containers {
     pub use dtz_containers::*;
 }
 
 #[cfg(feature = "core")]
-mod core {
+pub mod core {
     pub use dtz_core::*;
 }
 
 #[cfg(feature = "identity")]
-mod identity {
+pub mod identity {
     pub use dtz_identity::*;
 }
 
 #[cfg(feature = "objectstore")]
-mod objectstore {
+pub mod objectstore {
     pub use dtz_objectstore::*;
 }
 
 #[cfg(feature = "observability")]
-mod observability {
+pub mod observability {
     pub use dtz_observability::*;
 }
 
 #[cfg(feature = "rss2email")]
-mod rss2email {
+pub mod rss2email {
     pub use dtz_rss2email::*;
 }
