d6f213550e02358a6b63e7ff66e2236621646d1e
Jens Walter <jens@apimeister.com>
2024-07-18 13:13:40 +0200

    update

--- dtz-identifier/src/apikey_id.rs
+++ dtz-identifier/src/apikey_id.rs
@@ -5,6 +5,12 @@ pub struct ApiKeyId {
     pub id: uuid::Uuid,
 }
 
+impl Default for ApiKeyId {
+    fn default() -> Self {
+        Self { id: uuid::Uuid::now_v7() }
+    }
+}
+
 impl std::fmt::Display for ApiKeyId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!("{PREFIX}{}", self.id))
@@ -55,4 +61,45 @@ impl From<uuid::Uuid> for ApiKeyId {
     fn from(id: uuid::Uuid) -> Self {
         ApiKeyId { id }
     }
+}
+
+impl TryFrom<&str> for ApiKeyId {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if let Some(uuid_str) = value.strip_prefix(PREFIX) {
+            let uuid = uuid::Uuid::parse_str(uuid_str).map_err(|_e| "invalid format".to_string())?;
+            Ok(ApiKeyId { id: uuid })
+        } else {
+            Err("invalid format".to_string())
+        }
+    }
+}
+
+#[test]
+fn key_invalid_1(){
+    let k = "abc-dsfdg";
+    let apikey: Result<ApiKeyId,String> = ApiKeyId::try_from(k);
+    assert!(apikey.is_err())
+}
+
+#[test]
+fn key_invalid_2(){
+    let k = "apikey-dsfdg";
+    let apikey: Result<ApiKeyId,String> = ApiKeyId::try_from(k);
+    assert!(apikey.is_err())
+}
+
+#[test]
+fn key_valid_1(){
+    let k = "apikey-0190c589-eb70-7980-97cf-af67b3a84116";
+    let apikey: Result<ApiKeyId,String> = ApiKeyId::try_from(k);
+    assert!(apikey.is_ok())
+}
+
+#[test]
+fn key_invalid_3(){
+    let k = "abc-0190c589-eb70-7980-97cf-af67b3a84116";
+    let apikey: Result<ApiKeyId,String> = ApiKeyId::try_from(k);
+    assert!(apikey.is_err())
 }
