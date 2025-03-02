d135736cf0bf2531f88b8e1c05fe86a1e5179bba
Jens Walter <jens@apimeister.com>
2025-03-02 17:53:11 +0100

    upd

---
+++ dtz-identifier/src/case_id.rs
@@ -0,0 +1,92 @@
+static PREFIX: &str = "case-";
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CaseId {
+    pub id: String,
+}
+
+impl Default for CaseId {
+    fn default() -> Self {
+        Self {
+            id: crate::generate_internal_id(),
+        }
+    }
+}
+
+impl std::fmt::Display for CaseId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("{PREFIX}{}", self.id))
+    }
+}
+
+impl TryFrom<String> for CaseId {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if let Some(id_str) = value.strip_prefix(PREFIX) {
+            Ok(CaseId { id: id_str.to_string() })
+        } else {
+            Err("invalid format".to_string())
+        }
+    }
+}
+
+impl TryFrom<&str> for CaseId {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if let Some(id_str) = value.strip_prefix(PREFIX) {
+            Ok(CaseId { id: id_str.to_string() })
+        } else {
+            Err("invalid format".to_string())
+        }
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for CaseId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct CaseIdVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for CaseIdVisitor {
+            type Value = CaseId;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a string starting with '")?;
+                formatter.write_str(PREFIX)?;
+                formatter.write_str("' followed by a 8-character alphanumeric string")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                if let Some(uuid_str) = value.strip_prefix(PREFIX) {
+                    let uuid = uuid::Uuid::parse_str(uuid_str).map_err(E::custom)?;
+                    Ok(CaseId { id: uuid.to_string() })
+                } else {
+                    Err(E::custom("invalid format"))
+                }
+            }
+        }
+
+        deserializer.deserialize_str(CaseIdVisitor)
+    }
+}
+
+impl serde::Serialize for CaseId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+#[test]
+fn test_from() {
+    let ctx = CaseId::try_from("case-12345678").unwrap();
+    assert_eq!(ctx.id, "12345678");
+}
--- dtz-identifier/src/lib.rs
+++ dtz-identifier/src/lib.rs
@@ -1,4 +1,5 @@
 mod apikey_id;
+mod case_id;
 mod context_id;
 mod execution_id;
 mod feed_id;
@@ -10,16 +11,49 @@ mod service_id;
 mod task_id;
 
 pub use apikey_id::*;
+pub use case_id::*;
 pub use context_id::*;
 pub use execution_id::*;
 pub use feed_id::*;
 pub use identity_id::*;
 pub use job_id::*;
 pub use object_id::*;
+use rand::distr::{
+    uniform::{UniformChar, UniformSampler},
+    Uniform,
+};
 pub use role_id::*;
 pub use service_id::*;
 pub use task_id::*;
 
 pub trait ShortId {
     fn to_short_id(&self) -> String;
+}
+
+fn generate_internal_id() -> String {
+    const DEFAULT_LENGTH: usize = 8;
+    use rand::prelude::*;
+    let mut rng = rand::rng();
+    // generate the first non-numeric character
+    let first_char: char = loop {
+        let c: char = rng.sample(rand::distr::Alphanumeric) as char;
+        if c.is_alphabetic() {
+            break c;
+        }
+    };
+    // generate DEFAULT_LENGTH-1 random alphanumeric characters
+    let mut id: Vec<char> = (0..DEFAULT_LENGTH - 1)
+        .map(|_| rng.sample(rand::distr::Alphanumeric) as char)
+        .collect();
+    id.insert(0, first_char);
+    id.into_iter().collect::<String>().to_lowercase()
+}
+
+#[test]
+fn test_generate_id_string() {
+    let id = generate_internal_id();
+    println!("id: {}", id);
+    assert_eq!(id.len(), 8);
+    let c = id.chars().next().unwrap();
+    assert!(c.is_alphabetic());
 }
